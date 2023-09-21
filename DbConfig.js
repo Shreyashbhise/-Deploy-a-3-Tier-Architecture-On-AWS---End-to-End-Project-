ca99632b5f22ff592c5e2eee2e74526fee2d2df9
Shreyash Bhise <108046802+Shreyashbhise@users.noreply.github.com>
2023-09-21 06:27:48 -0400

    Update DbConfig.js

--- DbConfig.js
+++ DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'database-1-instance-1.cctljmzp8whn.us-east-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : '12345678',
-    DB_DATABASE : 'webappdb'
+    DB_HOST : '',
+    DB_USER : '',
+    DB_PWD : '',
+    DB_DATABASE : ''
 });
