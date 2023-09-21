446ed4fb7b8cf67618f3a67ca49c22ff05fe2ee6
shreyashbhise16 <shreyashbhise16@gmail.com>
2023-09-21 06:23:11 -0400

    file is added

---
+++ DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.cctljmzp8whn.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : '12345678',
+    DB_DATABASE : 'webappdb'
+});
